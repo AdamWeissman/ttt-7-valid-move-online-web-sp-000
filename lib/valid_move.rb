d7b035a7ab5e51738d3a965965ecfc1c5927f9c4
Adam Weissman <adam@comedytragedyepic.com>
2019-09-04 10:12:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,11 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  (position_not_taken?(board, index) && index.between?(0,8)) ? true : false
+  (position_not_taken?(board, index) && index.between?(0,8)) ? true : nil
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def position_not_taken?(board, index)
-    (board[index] != " " || board[index] != "" || board[index] != nil) ? true : false
+    (board[index] != " " || board[index] != "" || board[index] != nil) ? true : nil
 end
