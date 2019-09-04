8428a24fef463d7b6953ff30ac1c2b3d9841f547
Adam Weissman <adam@comedytragedyepic.com>
2019-09-04 10:20:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,9 +6,13 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_not_taken?(board, index)
+#def position_not_taken?(board, index)
     (board[index] != " " || board[index] != "" || board[index] != nil) ? true : false
 end
+
+position_taken?(board, index)
+
+
 
 
 x = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
